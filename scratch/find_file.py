75ea5b2fa48f7ebf983760622c7d153c95a234bc
philem247 <adeoyephilip247@gmail.com>
2026-06-03 19:10:38 +0100

    chore: untrack scratch folder and ignore it in .gitignore

--- scratch/find_file.py
+++
@@ -1,8 +0,0 @@
-import os
-
-target = "VideoViewModule.kt"
-node_modules_dir = r"C:\Users\hp\SabiSpell-V1\SabiSpell\node_modules\expo-av"
-
-for root, dirs, files in os.walk(node_modules_dir):
-    if target in files:
-        print("Found file at:", os.path.join(root, target))
